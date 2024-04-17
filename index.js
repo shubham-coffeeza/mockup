f71cfd865d9a6b61082984d0efccdb2ef536b2bf
Shubham Shankar Sawant <75775348+Shu13ham@users.noreply.github.com>
2024-04-17 16:38:25 +0530

    brands

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     
+
+function isMobileDevice() {
+    return /Mobile|Android/i.test(navigator.userAgent);
+}
+
 //screen loader start
     document.body.classList.add('loaded');
 //screen loader end
@@ -22,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Function to check initial scroll position on page load
     function checkInitialScrollPosition() {
-        if (document.querySelector('header').scrollY > 10) {
-        handleScroll(); // Execute the action if already scrolled beyond the threshold
+        if (window.scrollY > 100) {
+            document.querySelector('header').classList.add('already-scrolled');
         }
     }
 
@@ -123,5 +128,87 @@ document.querySelector(".close-btn").addEventListener("click", function(){
 });
 //hamburger
 
+
+//Brand Slider
+let currentSlide = 0;
+const slides = document.querySelectorAll(".brand");
+const totalSlides = slides.length/2;
+const slideWidth = slides[0].clientWidth;
+let startSliderX = 0;
+let endX = 0;
+
+//Show slide by index
+function showSlide(index) {
+  moveSlide(index);
+}
+
+//use transform for slides
+function moveSlide(index) {
+  const slideOffset = -index * slideWidth;
+  if(isMobileDevice()){
+    document.querySelector(".brand-wrapper").style.transform = `translateX(${slideOffset*2.5}px)`;
+  }
+  else
+    document.querySelector(".brand-wrapper").style.transform = `translateX(${slideOffset}px)`;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+  // If the next slide is the first slide, reset the transform to ensure looping
+  if (currentSlide === (totalSlides/2)) {
+    setTimeout(() => {
+    //   document.querySelector(".brand-wrapper").style.transition = 'none';
+      document.querySelector(".brand-wrapper").style.transform = `translateX(0px)`;
+      setTimeout(() => {
+        document.querySelector(".brand-wrapper").style.transition = '';
+      }, 50);
+    }, 500);
+  }
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+
+  // If the previous slide is the last slide, reset the transform to ensure looping
+  if (currentSlide === totalSlides - 1) {
+    setTimeout(() => {
+    //   document.querySelector(".brand-wrapper").style.transition = 'none';
+      document.querySelector(".brand-wrapper").style.transform = `translateX(-${totalSlides-1 * slideWidth}px)`;
+      setTimeout(() => {
+        document.querySelector(".brand-wrapper").style.transition = '';
+      }, 50);
+    }, 500);
+  }
+}
+
+document.querySelector(".prev").addEventListener("click", prevSlide);
+document.querySelector(".next").addEventListener("click", nextSlide);
+
+//Drag functionality for touch screens
+function startDrag(event) {
+  startSliderX = event.touches[0].clientX;
+}
+
+function drag(event) {
+  endX = event.touches[0].clientX;
+}
+
+function endDrag() {
+  const sensitivity = 50; // Adjust this value to control the sensitivity of the drag
+  const dragDistance = startSliderX - endX;
+
+  if (dragDistance > sensitivity) {
+    nextSlide(); // Move to next slide
+  } else if (dragDistance < -sensitivity) {
+    prevSlide(); // Move to previous slide
+  }
+}
+
+document.querySelector(".brand-wrapper").addEventListener("touchstart", startDrag);
+document.querySelector(".brand-wrapper").addEventListener("touchmove", drag);
+document.querySelector(".brand-wrapper").addEventListener("touchend", endDrag);
+//Brand Slider
     
 });
