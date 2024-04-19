f3ea2523ba92e8a03575b98d0fd153b5eefa88ec
Shubham Shankar Sawant <75775348+Shu13ham@users.noreply.github.com>
2024-04-19 16:13:29 +0530

    custom slider with overflow technique

--- index.js
+++ index.js
@@ -1,320 +1,199 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+  function isMobileDevice() {
+      return /Mobile|Android/i.test(navigator.userAgent);
+  }
 
-function isMobileDevice() {
-    return /Mobile|Android/i.test(navigator.userAgent);
-}
-
-function isTabletDevice() {
-  return /iPad/i.test(navigator.userAgent) || (/(tablet|ipad|playbook|silk)|(android(?!.*mobile))/i.test(navigator.userAgent));
-}
-//screen loader start
-    document.body.classList.add('loaded');
-//screen loader end
+  function isTabletDevice() {
+    return /iPad/i.test(navigator.userAgent) || (/(tablet|ipad|playbook|silk)|(android(?!.*mobile))/i.test(navigator.userAgent));
+  }
+  //screen loader start
+  document.body.classList.add('loaded');
+  //screen loader end
 
 //brand logo animation start
-    window.addEventListener('scroll', function() {
-        var targetElement = document.querySelector('header');
-        var currentScrollPosition = window.scrollY;
-        if (currentScrollPosition > 36) {
-            targetElement.classList.add('scrolled');
-        } else {
-            targetElement.classList.remove('scrolled');
-        }
-        if (currentScrollPosition > 100) {
-            document.querySelector('.banner-wrapper').classList.add('scrolled-pass-100');
-        } else {
-            document.querySelector('.banner-wrapper').classList.remove('scrolled-pass-100');
-        }
-    });
+  window.addEventListener('scroll', function() {
+      var targetElement = document.querySelector('header');
+      var currentScrollPosition = window.scrollY;
+      if (currentScrollPosition > 36) {
+          targetElement.classList.add('scrolled');
+      } else {
+          targetElement.classList.remove('scrolled');
+      }
+      if (currentScrollPosition > 100) {
+          document.querySelector('.banner-wrapper').classList.add('scrolled-pass-100');
+      } else {
+          document.querySelector('.banner-wrapper').classList.remove('scrolled-pass-100');
+      }
+  });
 
-    // Function to check initial scroll position on page load
-    function checkInitialScrollPosition() {
-        if (window.scrollY > 100) {
-            document.querySelector('header').classList.add('already-scrolled');
-        }
-    }
+  // Function to check initial scroll position on page load
+  function checkInitialScrollPosition() {
+      if (window.scrollY > 100) {
+          document.querySelector('header').classList.add('already-scrolled');
+      }
+  }
 
   // Check initial scroll position when the page loads
   checkInitialScrollPosition();
     
-    // setTimeout(function() {
-    //     document.querySelector('header').classList.add('scrolled');
-    // }, 4000);
+  // setTimeout(function() {
+  //     document.querySelector('header').classList.add('scrolled');
+  // }, 4000);
 //brand logo animation end
 
 //floating link start
-    var isOpen = false;
-    var startX, startY;
-    var threshold = 50; // Minimum distance to be considered a swipe
-    
-    // Event listener for the floating link button click
-    document.querySelector(".floating-link-btn").addEventListener("click", function(){
-        if (!isOpen) {
-            openFloatingLinkWindow();
-        } else {
-            closeFloatingLinkWindow();
-        }
-    });
-    
-    // Function to open the floating link window
-    function openFloatingLinkWindow() {
-        document.querySelector(".floating-link-window").style.transform = "translate(0, -50%)";
-        document.querySelector(".floating-link-btn").style.transform = "translateY(-50%) rotate(180deg)";
-        isOpen = true;
-    }
-    
-    // Function to close the floating link window
-    function closeFloatingLinkWindow() {
-        document.querySelector(".floating-link-window").style.transform = "translate(240px, -50%)";
-        document.querySelector(".floating-link-btn").style.transform = "translateY(-50%) rotate(0deg)";
-        isOpen = false;
-    }
-    
-    // Event listener for touch start
-    document.querySelector(".floating-link-btn").addEventListener("touchstart", function(e) {
-        startX = e.touches[0].clientX;
-        startY = e.touches[0].clientY;
-    });
-    
-    // Event listener for touch end
-    document.querySelector(".floating-link-btn").addEventListener("touchend", function(e) {
-        var endX = e.changedTouches[0].clientX;
-        var endY = e.changedTouches[0].clientY;
-        
-        var deltaX = endX - startX;
-        var deltaY = endY - startY;
-        
-        // Check if the swipe is horizontal and greater than the threshold
-        if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > threshold) {
-            if (deltaX > 0) {
-                closeFloatingLinkWindow();
-            } else {
-                openFloatingLinkWindow();
-            }
-        }
-    });
+  var isOpen = false;
+  var startX, startY;
+  var threshold = 50; // Minimum distance to be considered a swipe
+  
+  // Event listener for the floating link button click
+  document.querySelector(".floating-link-btn").addEventListener("click", function(){
+      if (!isOpen) {
+          openFloatingLinkWindow();
+      } else {
+          closeFloatingLinkWindow();
+      }
+  });
+  
+  // Function to open the floating link window
+  function openFloatingLinkWindow() {
+      document.querySelector(".floating-link-window").style.transform = "translate(0, -50%)";
+      document.querySelector(".floating-link-btn").style.transform = "translateY(-50%) rotate(180deg)";
+      isOpen = true;
+  }
+  
+  // Function to close the floating link window
+  function closeFloatingLinkWindow() {
+      document.querySelector(".floating-link-window").style.transform = "translate(240px, -50%)";
+      document.querySelector(".floating-link-btn").style.transform = "translateY(-50%) rotate(0deg)";
+      isOpen = false;
+  }
+  
+  // Event listener for touch start
+  document.querySelector(".floating-link-btn").addEventListener("touchstart", function(e) {
+      startX = e.touches[0].clientX;
+      startY = e.touches[0].clientY;
+  });
+  
+  // Event listener for touch end
+  document.querySelector(".floating-link-btn").addEventListener("touchend", function(e) {
+      var endX = e.changedTouches[0].clientX;
+      var endY = e.changedTouches[0].clientY;
+      
+      var deltaX = endX - startX;
+      var deltaY = endY - startY;
+      
+      // Check if the swipe is horizontal and greater than the threshold
+      if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > threshold) {
+          if (deltaX > 0) {
+              closeFloatingLinkWindow();
+          } else {
+              openFloatingLinkWindow();
+          }
+      }
+  });
 //floating link start
 
 
 //Coupon Btn
     
-    document.querySelector(".coupon-btn-wrapper").addEventListener("click", function(){
-        document.querySelector(".coupon").classList.add("active");
-    });
+  document.querySelector(".coupon-btn-wrapper").addEventListener("click", function(){
+      document.querySelector(".coupon").classList.add("active");
+  });
 
-    document.querySelector("main").addEventListener("click", function(){
-        document.querySelector(".coupon").classList.remove("active");
-    });
+  document.querySelector("main").addEventListener("click", function(){
+      document.querySelector(".coupon").classList.remove("active");
+  });
 
-    document.querySelector("footer").addEventListener("click", function(){
-        document.querySelector(".coupon").classList.remove("active");
-    });
+  document.querySelector("footer").addEventListener("click", function(){
+      document.querySelector(".coupon").classList.remove("active");
+  });
 
-    document.querySelector(".header-banner-wrapper").addEventListener("click", function(){
-        document.querySelector(".coupon").classList.remove("active");
-    });
+  document.querySelector(".header-banner-wrapper").addEventListener("click", function(){
+      document.querySelector(".coupon").classList.remove("active");
+  });
     
 //Coupon Btn
 
 //hamburger
-var hamburgerIsOpen = false;
+  var hamburgerIsOpen = false;
 
-document.querySelector(".hamburger").addEventListener("click", function(){
-    document.querySelector(".navbar-mobile").classList.add("hamburger-active");
-    hamburgerIsOpen = true;
-});
+  document.querySelector(".hamburger").addEventListener("click", function(){
+      document.querySelector(".navbar-mobile").classList.add("hamburger-active");
+      hamburgerIsOpen = true;
+  });
 
-document.querySelector(".close-btn").addEventListener("click", function(){
-    document.querySelector(".navbar-mobile").classList.remove("hamburger-active");
-    hamburgerIsOpen = false;
-});
+  document.querySelector(".close-btn").addEventListener("click", function(){
+      document.querySelector(".navbar-mobile").classList.remove("hamburger-active");
+      hamburgerIsOpen = false;
+  });
 //hamburger
 
 
-//Brand Slider
-let currentSlide = 0;
-const slides = document.querySelectorAll(".brand");
-const totalSlides = slides.length/2;
-const slideWidth = slides[0].clientWidth;
-let startSliderX = 0;
-let endX = 0;
+//brands slider
+const container = document.querySelector('.brand-wrapper');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
 
-//Show slide by index
-function showSlide(index) {
-  moveSlide(index);
+// Function to scroll the container to the left
+function scrollLeft() {
+  container.scrollBy({
+    left: -container.offsetWidth, // Scroll by one container width
+    behavior: 'smooth'
+  });
 }
 
-//use transform for slides
-function moveSlide(index) {
-  const slideOffset = -index * slideWidth;
-  if(isMobileDevice()){
-    document.querySelector(".brand-wrapper").style.transform = `translateX(${slideOffset*2.5}px)`;
-  }
-  else
-    document.querySelector(".brand-wrapper").style.transform = `translateX(${slideOffset}px)`;
+// Function to scroll the container to the right
+function scrollRight() {
+  container.scrollBy({
+    left: container.offsetWidth, // Scroll by one container width
+    behavior: 'smooth'
+  });
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % totalSlides;
-  showSlide(currentSlide);
-  // If the next slide is the first slide, reset the transform to ensure looping
-  if (currentSlide === (totalSlides/2)) {
-    setTimeout(() => {
-    //   document.querySelector(".brand-wrapper").style.transition = 'none';
-      document.querySelector(".brand-wrapper").style.transform = `translateX(0px)`;
-      setTimeout(() => {
-        document.querySelector(".brand-wrapper").style.transition = '';
-      }, 50);
-    }, 500);
-  }
+// Event listeners for navigation buttons
+prevBtn.addEventListener('click', scrollLeft);
+nextBtn.addEventListener('click', scrollRight);
+
+// Automatic scrolling
+const scrollInterval = 3000; // Interval in milliseconds
+let scrollTimer;
+
+function startAutoScroll() {
+  scrollTimer = setInterval(scrollRight, scrollInterval);
 }
 
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-  showSlide(currentSlide);
-
-  // If the previous slide is the last slide, reset the transform to ensure looping
-  if (currentSlide === totalSlides - 1) {
-    setTimeout(() => {
-    //   document.querySelector(".brand-wrapper").style.transition = 'none';
-      document.querySelector(".brand-wrapper").style.transform = `translateX(-${totalSlides-1 * slideWidth}px)`;
-      setTimeout(() => {
-        document.querySelector(".brand-wrapper").style.transition = '';
-      }, 50);
-    }, 500);
-  }
+function stopAutoScroll() {
+  clearInterval(scrollTimer);
 }
 
-document.querySelector(".prev").addEventListener("click", prevSlide);
-document.querySelector(".next").addEventListener("click", nextSlide);
+// Start auto-scrolling
+startAutoScroll();
 
-//Drag functionality for touch screens
-function startDrag(event) {
-  startSliderX = event.touches[0].clientX;
-}
-
-function drag(event) {
-  endX = event.touches[0].clientX;
-}
-
-function endDrag() {
-  const sensitivity = 50; // Adjust this value to control the sensitivity of the drag
-  const dragDistance = startSliderX - endX;
-
-  if (dragDistance > sensitivity) {
-    nextSlide(); // Move to next slide
-  } else if (dragDistance < -sensitivity) {
-    prevSlide(); // Move to previous slide
-  }
-}
-
-document.querySelector(".brand-wrapper").addEventListener("touchstart", startDrag);
-document.querySelector(".brand-wrapper").addEventListener("touchmove", drag);
-document.querySelector(".brand-wrapper").addEventListener("touchend", endDrag);
-//Brand Slider
-
-//Press Slider
-let currentSlidePress = 0;
-const slidesPress = document.querySelectorAll(".press");
-const totalSlidesPress = slidesPress.length/2;
-const slideWidthPress = slidesPress[0].clientWidth;
-let startSliderXPress = 0;
-let endXPress = 0;
-
-//Show slide by index
-function showSlidePress(index) {
-  moveSlidePress(index);
-}
-
-//use transform for slidesPress
-function moveSlidePress(index) {
-  const slideOffset = -index * slideWidthPress;
-  if(isMobileDevice()){
-    document.querySelector(".press-wrapper").style.transform = `translateX(${slideOffset*2.5}px)`;
-  }
-  else
-    document.querySelector(".press-wrapper").style.transform = `translateX(${slideOffset}px)`;
-}
-
-function nextSlidePress() {
-  currentSlidePress = (currentSlidePress + 1) % totalSlidesPress;
-  showSlidePress(currentSlidePress);
-  // If the next slide is the first slide, reset the transform to ensure looping
-  if (currentSlidePress === (totalSlidesPress/2)) {
-    setTimeout(() => {
-    //   document.querySelector(".brand-wrapper").style.transition = 'none';
-      document.querySelector(".press-wrapper").style.transform = `translateX(0px)`;
-      setTimeout(() => {
-        document.querySelector(".press-wrapper").style.transition = '';
-      }, 50);
-    }, 500);
-  }
-}
-
-function prevSlidePress() {
-  currentSlidePress = (currentSlidePress - 1 + totalSlidesPress) % totalSlidesPress;
-  showSlidePress(currentSlidePress);
-
-  // If the previous slide is the last slide, reset the transform to ensure looping
-  if (currentSlidePress === totalSlidesPress - 1) {
-    setTimeout(() => {
-    //   document.querySelector(".brand-wrapper").style.transition = 'none';
-      document.querySelector(".press-wrapper").style.transform = `translateX(-${totalSlidesPress-1 * slideWidthPress}px)`;
-      setTimeout(() => {
-        document.querySelector(".press-wrapper").style.transition = '';
-      }, 50);
-    }, 500);
-  }
-}
-
-document.querySelector(".press-prev").addEventListener("click", prevSlidePress);
-document.querySelector(".press-next").addEventListener("click", nextSlidePress);
-
-//Drag functionality for touch screens
-function startDragPress(event) {
-  startSliderXPress = event.touches[0].clientX;
-}
-
-function dragPress(event) {
-  endXPress = event.touches[0].clientX;
-}
-
-function endDragPress() {
-  const sensitivity = 50; // Adjust this value to control the sensitivity of the drag
-  const dragDistance = startSliderXPress - endXPress;
-
-  if (dragDistance > sensitivity) {
-    nextSlidePress(); // Move to next slide
-  } else if (dragDistance < -sensitivity) {
-    prevSlidePress(); // Move to previous slide
-  }
-}
-
-document.querySelector(".press-wrapper").addEventListener("touchstart", startDragPress);
-document.querySelector(".press-wrapper").addEventListener("touchmove", dragPress);
-document.querySelector(".press-wrapper").addEventListener("touchend", endDragPress);
-//press Slider
+// Pause auto-scrolling when hovering over the container
+container.addEventListener('mouseenter', stopAutoScroll);
+container.addEventListener('mouseleave', startAutoScroll);
+//brands slider
 
 //Exit intent popup
-// Check if the exit intent popup has been shown in the last 5 hours
-function canShowExitIntentPopup() {
-  var lastClosedTime = localStorage.getItem("exitIntentLastClosedTime");
-  if (!lastClosedTime) {
-      return true; // Popup has not been closed before
-  } else {
-      var fiveHoursAgo = Date.now() - (5 * 60 * 60 * 1000); // Calculate the time 5 hours ago
-      return parseInt(lastClosedTime) < fiveHoursAgo;
+  // Check if the exit intent popup has been shown in the last 5 hours
+  function canShowExitIntentPopup() {
+    var lastClosedTime = localStorage.getItem("exitIntentLastClosedTime");
+    if (!lastClosedTime) {
+        return true; // Popup has not been closed before
+    } else {
+        var fiveHoursAgo = Date.now() - (5 * 60 * 60 * 1000); // Calculate the time 5 hours ago
+        return parseInt(lastClosedTime) < fiveHoursAgo;
+    }
   }
-}
 
-document.addEventListener("mouseleave", function(event) {
-  if (event.clientY < 0 && canShowExitIntentPopup()) {
-      document.querySelector(".exit-intent-popup").style.display = "flex";
-  }
-});
+  document.addEventListener("mouseleave", function(event) {
+    if (event.clientY < 0 && canShowExitIntentPopup()) {
+        document.querySelector(".exit-intent-popup").style.display = "flex";
+    }
+  });
 
-document.querySelector(".exit-intent-popup, .exit-intent-popup .close").addEventListener("click", function(){
+  document.querySelector(".exit-intent-popup, .exit-intent-popup .close").addEventListener("click", function(){
   document.querySelector(".exit-intent-popup").style.display = "none";
   
   // Save the current time when the popup is closed
@@ -323,7 +202,7 @@ document.querySelector(".exit-intent-popup, .exit-intent-popup .close").addEvent
 //Exit intent popup
 
 //Third Section
-var parentElement = document.querySelector('.third-section');
+  var parentElement = document.querySelector('.third-section');
   var blockElement = document.querySelector('.third-section img');
   var detailElement = document.querySelector('.details-wrapper');
 
